a3d3cd63bccf94ca7ac8d8c2a8970a59ca472ae3
matthiasheck <matthias.heck.gm@gmail.com>
2015-10-27 22:35:07 +0100

    cleaned up and 1 bug removed

---
+++ src/main/scala/TwirlSupport.scala
@@ -0,0 +1,22 @@
+import play.twirl.api.{Xml, Txt, Html}
+import akka.http.scaladsl.marshalling.{Marshaller, ToEntityMarshaller}
+import akka.http.scaladsl.model.MediaTypes._
+import akka.http.scaladsl.model.ContentType
+
+trait TwirlSupport {
+  //https://github.com/btomala/akka-http-twirl
+
+  /** Serialize Twirl `Html` to `text/html`. */
+  implicit val twirlHtmlMarshaller = twirlMarshaller[Html](`text/html`)
+
+  /** Serialize Twirl `Txt` to `text/plain`. */
+  implicit val twirlTxtMarshaller = twirlMarshaller[Txt](`text/plain`)
+
+  /** Serialize Twirl `Xml` to `text/xml`. */
+  implicit val twirlXmlMarshaller = twirlMarshaller[Xml](`text/xml`)
+
+  /** Serialize Twirl formats to `String`. */
+  protected def twirlMarshaller[A <: AnyRef : Manifest](contentType: ContentType): ToEntityMarshaller[A] =
+    Marshaller.StringMarshaller.wrap(contentType)(_.toString)
+
+}
---
+++ tutorial/testJson.scala
@@ -0,0 +1,57 @@
+import akka.http.scaladsl.server.Directives._
+import spray.json._
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+
+case class Adress(streetname: String, plz: BigDecimal, city: String)
+
+case class Country(country: String, isAgent: Boolean)
+
+
+trait AddressJsonProtocol extends DefaultJsonProtocol {
+  implicit val formatAddress = jsonFormat3(Adress.apply)
+  implicit val formatSC = jsonFormat2(Country.apply)
+}
+
+object Main extends App with AddressJsonProtocol {
+
+  val jsonString =
+  """
+    |[
+    |{"name": "HK", "zahl": 0},
+    |{"name": "YE", "zahl": 1}
+    |]
+  """.stripMargin
+//  val jsonString =
+//    """
+//      |[
+//      |["HK", 0],
+//      |["PS", 1],
+//      |["PT", 0],
+//      |["HN", 1],
+//      |["PY", 1],
+//      |["HR", 0],
+//      |["YE", 1]
+//      |]
+//    """.stripMargin
+
+
+  val address = new Adress("street", 123, "city")
+  println("poso's default toString: %s".format(address))
+  val addressJVal = address.toJson
+  println("JValue's toString: %s".format(addressJVal))
+  val addressJson = addressJVal.prettyPrint
+  println("pretty-printing: %s".format(addressJson))
+
+
+  val listAddresses = List(address, address)
+
+
+
+//  println(jsonString.parseJson.convertTo[List[Country]] )
+
+
+
+  println("prPPR: %s".format(ttt))
+
+}
+
